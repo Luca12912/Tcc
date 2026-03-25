247301c9c14ce73954eae303e3c0bd12d4df7c94
Luca12912 <2409214@escolas.anchieta.br>
2026-03-25 11:45:43 -0300

    Create CustomerPage component

---
+++ frontend/src/pages/CustomerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuCard from '../components/MenuCard';
+
+const CustomerPage = () => {
+    return (
+        <div>
+            <h1>Customer Menu</h1>
+            <MenuCard />
+            {/* Add Cart Component Here */}
+        </div>
+    );
+};
+
+export default CustomerPage;
